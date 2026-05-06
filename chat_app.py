c47e0865bed1e408a9fbf32fb8bc5c96ecadbc0b
Manal Batta <manal.batta.1234@gmail.com>
2026-05-06 15:40:02 +0300

    First commit

---
+++ chat_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import random
+import time
+
+st.title("Echo Bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+            "Try to guess the next response?"
+        ]
+    )
+
+def streamer(response):
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.07)
+
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    st.write_stream(streamer(response))
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+
